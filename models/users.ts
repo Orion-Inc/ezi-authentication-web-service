47f07b3463542a1901a7d12f14979e341467c7a7
PyGeek <ankomah670@gmail.com>
2019-04-25 08:55:10 +0000

    models
    Updated the models folder

---
+++ models/users.ts
@@ -0,0 +1,34 @@
+import mongoose from "mongoose"
+import { default as Roles } from "./roles"
+
+const Schema = mongoose.Schema;
+
+const UserSchema = new Schema({
+    email: {
+        type: String,
+        required: true
+    },
+    password: {
+        type: String,
+        required: true
+    },
+    role_id: {
+        type: Schema.Types.ObjectId,
+        ref: 'Roles',
+    }
+},{
+    timestamps:{
+        createdAt: "created_at",
+        updatedAt: "updated_at"
+    }
+});
+
+UserSchema.add({
+    is_default: {
+        type: Boolean,
+        required: true
+    }
+});
+
+const User = mongoose.model("Users", UserSchema);
+export default User;
