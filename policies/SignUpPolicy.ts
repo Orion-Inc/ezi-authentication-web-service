de7d4af5055913cf52013186dd8dfd34b6d65adc
LORD KAY <offeilord@gmail.com>
2019-04-26 18:37:09 +0000

    :ok_hand: add the signup policy to the sign up route

---
+++ policies/SignUpPolicy.ts
@@ -0,0 +1,50 @@
+import { Request, Response, NextFunction } from "express";
+import * as Joi from "joi"
+class SignUpPolicy {
+    static registerPolicy = (req: Request, res: Response, next: NextFunction ) => {
+        // Input schema from the request body
+        const reqSchema = {
+            name: req.body.name,
+            email: req.body.email,
+            phone: req.body.phone
+        };
+
+        // definition schema for validation
+        const joiSchema = Joi.object().keys({
+            name: Joi.string().min(8).required(),
+            email: Joi.string().email({ minDomainAtoms: 2 }).required(),
+            phone: Joi.number().integer().min(10).required()
+        });
+
+        const { error , value } = Joi.validate(reqSchema, joiSchema);
+        if (error) {
+            switch (error.details[0].context.key) {
+                case 'name':
+                    res.status(403)
+                        .json({
+                            message: error.details[0].message,
+                            success: false
+                        });
+                    break;
+                case 'email':
+                    res.status(403)
+                        .json({
+                            message: error.details[0].message,
+                            success: false
+                        });
+                    break;
+                case 'phone':
+                    res.status(403)
+                        .json({
+                            message: error.details[0].message,
+                            success: false
+                        });
+                    break;
+            }
+        } else {
+            next();
+        }
+    }
+}
+
+export default SignUpPolicy;
