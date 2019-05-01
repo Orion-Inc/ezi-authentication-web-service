4b03d36e5c9c1334fbd167d2df2927cb07dc0428
LORD KAY <offeilord@gmail.com>
2019-05-01 08:16:14 +0000

    :construction: Start work on the sign up api endpoint

--- api/v1/auth/SignUp.ts
+++ api/v1/auth/SignUp.ts
@@ -1,4 +1,13 @@
+import { Router } from "express";
+import { default as SignUpPolicy } from "@policies/SignUpPolicy"
+import { default as SignUpController } from "@controllers/auth/SignUpController"
 
-export class SignUp {
-    
-}
+const signUpRouter = Router();
+signUpRouter.post('/signup',
+    SignUpPolicy.registerPolicy,
+    SignUpController.register
+    );
+
+
+// exporting the default configuration modules to be used by the aws
+export default signUpRouter;
