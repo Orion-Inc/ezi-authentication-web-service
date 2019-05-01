8a588bc51f77b08563dabadcd48a6d22fe717c5f
LORD KAY <offeilord@gmail.com>
2019-05-01 08:15:55 +0000

    :recycle: Refactor the codes from functions to class methods

--- server.ts
+++ server.ts
@@ -1,7 +1,5 @@
 "use strict";
 import { default as Server } from "./app"
-import http from "http";
-
 const server = new Server();
 // starting the server by doing the configurations here
 server.start();
