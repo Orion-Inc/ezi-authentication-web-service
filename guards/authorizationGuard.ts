1b14db46e09084021c69ceadb5c0781d6da8e249
LORD KAY <offeilord@gmail.com>
2019-05-01 08:14:05 +0000

    :recycle: make some changes to the authorization Guard configuration file

--- guards/authorizationGuard.ts
+++ guards/authorizationGuard.ts
@@ -1,5 +1,6 @@
 import * as jwt from "jsonwebtoken";
 import {NextFunction, Request, Response, Router} from "express";
+import config from "../config/config"
 const authorizationGuard = Router();
 
 class JWTAuth {
@@ -12,12 +13,32 @@ class JWTAuth {
     static checkToken = (req: Request, res: Response, next: NextFunction) => {
         const token: string = req.body.token || req.query.token || req.headers['x-access-token'] || req.headers['Authorization'];
         if (token) {
-
+            jwt.verify(token, config.secret,{
+                algorithms: ['HS256']
+            }, (err, decoded) => {
+                if (err) {
+                    res.status(401)
+                        .json({
+                            message: 'Authentication failed. Session Expired',
+                            success: false,
+                            results: err
+                        })
+                } else {
+                    res.locals.decoded = decoded;
+                    next();
+                }
+            });
+        } else {
+            res.status(403)
+                .json({
+                    message: 'No token provided',
+                    success: false
+                });
         }
     }
 }
 
 authorizationGuard.use(JWTAuth.checkToken);
 
-export = authorizationGuard;
+export default authorizationGuard;
 
