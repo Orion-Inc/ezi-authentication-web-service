f43abbe4902ce43d5d295f3c01181f100687e2c1
LORD KAY <offeilord@gmail.com>
2019-05-04 19:54:26 +0000

    Add a medium strength password regex

--- config/config.ts
+++ config/config.ts
@@ -1,3 +1,4 @@
 export default {
-    secret: 'H<!KJhCZy2Q{jNn[L!>r.FVkh\'5tf2H$4zI,Aa}o1H=$-45aMmKd=nAt`^e$m5'
+    secret: 'H<!KJhCZy2Q{jNn[L!>r.FVkh\'5tf2H$4zI,Aa}o1H=$-45aMmKd=nAt`^e$m5',
+    passwordStrength: '^(((?=.*[a-z])(?=.*[A-Z]))|((?=.*[a-z])(?=.*[0-9]))|((?=.*[A-Z])(?=.*[0-9])))(?=.{6,})'
 };
