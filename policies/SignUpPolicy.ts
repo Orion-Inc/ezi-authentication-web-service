12facae738d48d9b0c7e5d456430fcc2572744ec
LORD KAY <offeilord@gmail.com>
2019-05-04 19:54:01 +0000

    :lock: Add a password policy to the configurations

--- policies/SignUpPolicy.ts
+++ policies/SignUpPolicy.ts
@@ -1,22 +1,25 @@
-import { Request, Response, NextFunction } from "express";
+import {Request, Response, NextFunction} from "express";
 import * as Joi from "joi"
+
 class SignUpPolicy {
-    static registerPolicy = (req: Request, res: Response, next: NextFunction ) => {
+    static registerPolicy = (req: Request, res: Response, next: NextFunction) => {
         // Input schema from the request body
         const reqSchema = {
             name: <string> req.body.name,
             email: <string> req.body.email,
+            password: <string> req.body.password,
             phone: <string> req.body.phone
         };
 
         // definition schema for validation
         const joiSchema = Joi.object().keys({
             name: Joi.string().min(8).required(),
-            email: Joi.string().email({ minDomainAtoms: 2 }).required(),
+            email: Joi.string().email({minDomainAtoms: 2}).required(),
+            password: Joi.string().regex(/[0-9A-Za-z]+{,8}/).min(8).required(),
             phone: Joi.number().integer().min(10).required()
         });
 
-        const { error , value } = Joi.validate(reqSchema, joiSchema);
+        const {error, value} = Joi.validate(reqSchema, joiSchema);
         if (error) {
             switch (error.details[0].context.key) {
                 case 'name':
@@ -27,6 +30,13 @@ class SignUpPolicy {
                         });
                     break;
                 case 'email':
+                    res.status(403)
+                        .json({
+                            message: error.details[0].message,
+                            success: false
+                        });
+                    break;
+                case 'password':
                     res.status(403)
                         .json({
                             message: error.details[0].message,
