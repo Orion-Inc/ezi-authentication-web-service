b0500378b0d52afb5d886c79ebc2d8c503462c92
LORD KAY <offeilord@gmail.com>
2019-05-07 12:59:26 +0000

    :pencil2: fix a typo

--- test/test-conn/db.spec.ts
+++ test/test-conn/db.spec.ts
@@ -1,10 +1,10 @@
 import mongoose from "mongoose"
-import { expect } from "chai"
+import {expect} from "chai"
 import dotenv from "dotenv";
 
-dotenv.config({ path: "../../.env" });
+dotenv.config({path: "../../.env"});
 // writing a test suit for the connection with the database
-before(function(done) {
+before(function (done) {
     mongoose.connect("mongodb://127.0.0.1:27017", {
         dbName: "schools_testing",
         useNewUrlParser: true
@@ -12,17 +12,17 @@ before(function(done) {
     mongoose.set('useCreateIndex', true);
     const db = mongoose.connection;
     db.on('error', console.error.bind(console, "connection error"));
-    db.once('open', function() {
+    db.once('open', function () {
         done();
     });
 });
 
-after( function(done) {
+after(function (done) {
     mongoose.connection.close(done);
 });
 
 export default function () {
-    afterEach( function(done) {
+    afterEach(function (done) {
         mongoose.connection.db.dropDatabase(done);
     });
 }
