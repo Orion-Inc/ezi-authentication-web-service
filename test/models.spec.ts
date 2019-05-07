c88cbb8e3597890d56eb2565b7c387f14b9bca4a
LORD KAY <offeilord@gmail.com>
2019-05-07 12:58:32 +0000

    :white_check_mark: :heavy_plus_sign: add a dependency and test it

--- test/models.spec.ts
+++ test/models.spec.ts
@@ -2,6 +2,7 @@ import {default as Roles} from "../models/roles"
 import {default as Schools} from "../models/school"
 import {default as db} from "./test-conn/db.spec"
 import {expect} from "chai"
+import * as _ from "lodash";
 
 // make the connection before you start something
 db();
@@ -23,6 +24,23 @@ describe("Testing Role Model", function () {
             expect(results._id).to.exist;
             expect(results).to.be.an("object");
             expect(results).to.have.property("name");
+        });
+        done();
+    });
+
+    it('should save data to the schools model', function (done) {
+        const request = new Schools({
+            name: "Presec International",
+            email: "presec@gmail.com",
+            password: "Lordbanks@1996",
+            phone: _.toNumber("0200746418"),
+            is_basic: true,
+            is_secondary: false
+        });
+
+        request.save((err, results) => {
+            expect(results).to.have.property("_id");
+            expect(results).to.be.an("object");
         });
         done();
     });
