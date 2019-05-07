4b0600418663082927b07b39e02cea817637c955
LORD KAY <offeilord@gmail.com>
2019-05-07 08:42:31 +0000

    :white_check_mark: Test the policies for the signup controller. Password test == checked

--- test/endpoints.spec.ts
+++ test/endpoints.spec.ts
@@ -1,5 +1,5 @@
-import { expect } from "chai"
-import { default as Server } from "../app"
+import {expect} from "chai"
+import * as _ from "lodash"
 import request from "supertest"
 
 const server = request.agent("http://localhost:8088");
@@ -12,6 +12,28 @@ describe("GET /testing", function () {
                 if (err) return done(err);
                 expect(results).to.be.an("object");
                 expect(results.body.success).to.be.true;
+                done();
+            });
+    });
+});
+
+// testing the policies for the sign up endpoint
+describe("POST /auth/signup", function() {
+    it('should validate the password', function (done) {
+        server.
+            post("/api/v1/auth/signup")
+            .send({
+                name: "Presec International",
+                email: "presec@gmail.com",
+                password: "LordBanks@1996",
+                phone: _.toNumber("0200746418"),
+                is_basic: true,
+                is_secondary: false
+            })
+            .expect("Content-Type",/json/)
+            .expect(403)
+            .end((err, response) => {
+               console.log(response.body)
                 done();
             });
     });
