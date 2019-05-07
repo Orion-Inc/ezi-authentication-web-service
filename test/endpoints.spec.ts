a09a509e494235d593292ee4f9c1f79c0de8f97e
LORD KAY <offeilord@gmail.com>
2019-05-07 12:39:07 +0000

    Test a middleware hook

--- test/endpoints.spec.ts
+++ test/endpoints.spec.ts
@@ -3,6 +3,8 @@ import * as _ from "lodash"
 import request from "supertest"
 
 const server = request.agent("http://localhost:8088");
+
+// testing the /testing endpoint
 describe("GET /testing", function () {
     it('should return a testing message', function (done) {
          server
@@ -18,7 +20,7 @@ describe("GET /testing", function () {
 });
 
 // testing the policies for the sign up endpoint
-describe("POST /auth/signup", function() {
+describe("POST /auth/signup for validating input policies", function() {
     it('should validate against the policies', function (done) {
         server.
             post("/api/v1/auth/signup")
@@ -40,7 +42,8 @@ describe("POST /auth/signup", function() {
     });
 });
 
-describe("POST /auth/signup", function () {
+// this test returns the user of the associated school
+describe("POST /auth/signup for signing up", function () {
     it('should save data to the signup model', function (done) {
         server
             .post("/api/v1/auth/signup")
@@ -53,7 +56,7 @@ describe("POST /auth/signup", function () {
                 is_secondary: false
             })
             .expect("Content-Type",/json/)
-            .expect(200)
+            .expect(201)
             .end((err, response) => {
                 expect(response.body.results.email).to.equal("presec@gmail.com");
                 expect(response.body.results).to.have.property("_id");
