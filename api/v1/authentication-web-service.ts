a0473c5ddfb4d3b67da4152c881964a5c842e3a2
LORD KAY <offeilord@gmail.com>
2019-05-21 08:30:13 +0000

    Add the sign in export class to the api base path

--- api/v1/authentication-web-service.ts
+++ api/v1/authentication-web-service.ts
@@ -1,9 +1,10 @@
-import { default as signUpRouter } from "./auth/SignUp"
-import { Router } from "express"
-import { default as authorizationGuard } from "@guards/authorizationGuard"
+import {default as signUpRouter} from "./auth/SignUp"
+import {default as signInRouter} from "./auth/SignIn"
+import {Router} from "express"
+import {default as authorizationGuard} from "@guards/authorizationGuard"
 
 const auth_web_service =  Router();
-auth_web_service.use("/auth", [signUpRouter]);
+auth_web_service.use("/auth", [signUpRouter, signInRouter]);
 
 // setting up the authorization guard here
 auth_web_service.use(authorizationGuard);
