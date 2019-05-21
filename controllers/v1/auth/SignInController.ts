3d2b0a4662c41a458f10301aa3b5c8c918498a73
LORD KAY <offeilord@gmail.com>
2019-05-21 10:17:24 +0000

    Still working on the authentication configs

--- controllers/v1/auth/SignInController.ts
+++ controllers/v1/auth/SignInController.ts
@@ -3,11 +3,12 @@ import {Request, Response} from "express";
 class SignInController {
     static login = async (req: Request, res: Response) => {
         // getting the values from the locals in the res value
+
         res.status(200)
             .json({
                 message: 'Token generated successfully',
                 success: true,
-                results: res.locals.user
+                results: res.locals.role
             })
     }
 }
